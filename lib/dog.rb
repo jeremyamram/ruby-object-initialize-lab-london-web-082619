eb0b8394916ebeb343a4ff8981efaa094301a4b0
Jeremy Amram <jeremy.j.amram@gmail.com>
2019-08-28 14:54:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,6 @@
-1.	class Dog
-2.	  def initialize(name, breed)
-3.	    @name, @breed = name, breed
-4.	  end
-5.	 
-6.	  def name=(name,breed,"Mutt")
-7.	    @name = name
-        @breed = breed
-8.	  end
-9.	 
-10.	  def name
-11.	    @name
-12.	  end
-13.	end
+class	Dog 
+  def name 
+    @name
+  end
+  def name=(name)
+    @name=name
