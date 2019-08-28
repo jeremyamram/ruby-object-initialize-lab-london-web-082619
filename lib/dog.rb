351e1a41c04f5fa4e6490ed6339dd6913ab66a04
Jeremy Amram <jeremy.j.amram@gmail.com>
2019-08-28 14:46:29 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+1.	class Dog
+2.	  def initialize(breed)
+3.	    @breed = breed
+4.	  end
+5.	 
+6.	  def breed=(breed)
+7.	    @breed = breed
+8.	  end
+9.	 
+10.	  def breed
+11.	    @breed
+12.	  end
+13.	end
